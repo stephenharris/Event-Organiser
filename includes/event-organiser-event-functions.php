303987e4ad3cbced717d35d1de04faf0c313bfaa
Ben Huson <ben@thewhiteroom.net>
2012-07-17 12:03:49 +0100

    Quick fix for DateTimeZone cache.
    DateTimeZone must be configured in ini file in PHP 5.3.0+

--- includes/event-organiser-event-functions.php
+++ includes/event-organiser-event-functions.php
@@ -719,7 +719,8 @@ function eo_get_blog_timezone(){
 
 		$timezone = new DateTimeZone($tzstring);
 
-		wp_cache_set( 'eventorganiser_timezone', $timezone );
+		if ( $timezone->getOffset() )
+			wp_cache_set( 'eventorganiser_timezone', $timezone );
 	} 
 
 	return $timezone; 
