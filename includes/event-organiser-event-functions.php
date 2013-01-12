stephenharris/Event-Organiser

commit 7b0724e5b0014def9af3b9c928e05449a7d38788

Adds year, month and date based archives, with pretty url. Closes #32.

includes/event-organiser-event-functions.php
@@ -334,11 +334,12 @@ function eo_get_next_occurrence_of($post_id=0){
 	if( ! $nextoccurrence )
 		return false;
 
-	$start = new DateTime($nextoccurrence->StartDate.' '.$nextoccurrence->StartTime);
-	$end = new DateTime($nextoccurrence->EndDate.' '.$nextoccurrence->FinishTime);
+	$start = new DateTime($nextoccurrence->StartDate.' '.$nextoccurrence->StartTime, $tz);
+	$end = new DateTime($nextoccurrence->EndDate.' '.$nextoccurrence->FinishTime, $tz);
 
 	return compact('start','end');
 }
+
 
 /**
 * Prints the formated date of next occurrence of an event
@@ -880,5 +881,49 @@ function eventorganiser_get_event_meta_list( $post_id=0 ){
 	$html ='</ul>';
 
 	return apply_filters('eventorganiser_event_meta_list', $html, $post_id);
+}
+
+
+/**
+ * Returns an the link for the event archive
+ * 
+ * Optionally provide the year , or month or day to get an year/month/day archive link. 
+ * To get a month archive you should provide a year
+ * To get a day archive you should provide a year and month.
+ *
+ *@since 1.7 
+ *@param int $year Year in full format, e.g. 2018. Must be provide for date-based archive link.
+ *@param int $month Numeric representation of month. 1 = Jan, 12 = Dec. Must be provide for month or day archive link
+ *@param int $day Day of the month 1-31
+ *@return string Link to the requested archive
+ *
+*/
+function eo_get_event_archive_link( $year=false,$month=false, $day=false){
+	global $wp_rewrite;
+
+	$archive = get_post_type_archive_link('event');
+
+	if( $year == false && $month == false && $day == false )
+		return $archive;
+	
+	$_year = str_pad($year, 4, "0", STR_PAD_LEFT);
+	$_month = str_pad($month, 2, "0", STR_PAD_LEFT);
+	$_day = str_pad($day, 2, "0", STR_PAD_LEFT);
+
+	if( $day ){
+		$date = compact('_year','_month','_day');
+	}elseif( $month ){
+		$date = compact('_year','_month');
+	}else{
+		$date = compact('_year');
+	}
+
+	if( $archive && $wp_rewrite->using_mod_rewrite_permalinks() && $permastruct = $wp_rewrite->get_extra_permastruct('event_archive') ){
+		$archive = home_url( $wp_rewrite->front.str_replace('%event_ondate%',implode('/',$date), $permastruct ) );
+	}else{
+		$archive = add_query_arg('ondate',implode('-',$date),$archive);
+	}
+
+	return $archive;
 }
 ?>

classes/class-eo-calendar-widget.php
@@ -84,8 +84,12 @@ class EO_Calendar_Widget extends WP_Widget
 
 		//Set the month to display (DateTime must be 1st of that month)
 		$tz = eo_get_blog_timezone();
-		$date =  get_query_var('ondate') ?  get_query_var('ondate') : 'now';
-		$month = new DateTime($date,$tz);
+		$date =  get_query_var('ondate') ?  str_replace('/','-',get_query_var('ondate')) : 'now';
+		try{
+			$month = new DateTime($date,$tz);
+		}catch( Exception $e){
+			$month = new DateTime('now',$tz);
+		}
 		$month = date_create($month->format('Y-m-1'),$tz);
 	
 		/* Set up the event query */
@@ -226,7 +230,8 @@ function generate_output($month,$args=array()){
 					$class[] ='event';
 					$events = $calendar_events[$formated_date];
 
-					$link = esc_url(add_query_arg('ondate',$current_date->format('Y-m-d'),$event_archive_link));
+					$link = esc_url(eo_get_event_archive_link($current_date->format('Y'),$current_date->format('m'),$current_date->format('d')));
+
 					/**
 					 * Filters the the link of a date on the events widget calendar
 					 *@param string $link The link

includes/event-organiser-archives.php
@@ -20,6 +20,10 @@ function eventorganiser_register_query_vars( $qvars ){
 	$qvars[] = 'ondate';
 	$qvars[] = 'showrepeats';
 	$qvars[] = 'eo_interval';
+	$qvars[] = 'event_start_before';
+	$qvars[] = 'event_start_after';
+	$qvars[] = 'event_end_before';
+	$qvars[] = 'event_after_after';
 	return $qvars;
 }
 add_filter('query_vars', 'eventorganiser_register_query_vars' );
@@ -53,9 +57,30 @@ function eventorganiser_pre_get_posts( $query ) {
 
 	//If querying for all events starting on given date, set the date parameters
 	if( !empty($query->query_vars['ondate']) ) {
+
+		$ondate_start = str_replace('/','-',$query->query_vars['ondate']);
+		$ondate_end = str_replace('/','-',$query->query_vars['ondate']);
+
+		$parts = count(explode('-',$ondate_start));
+		if( $parts == 1 && is_numeric($ondate_start) ){
+			//Numeric - interpret as year
+			$ondate_start .= '-01-01';
+			$ondate_end .= '-12-31';
+		}elseif( $parts == 2 ){
+			// 2012-01 format: interpret as month
+			$ondate_start .= '-01';
+			try{
+				$end = new DateTime($ondate_start);
+				$ondate_end = $end->format('Y-m-t');
+			}catch( Exception $e){
+				$query->set('ondate',false);
+				break;
+			}
+		}
+	
 		$query->set('post_type', 'event');
-		$query->set('event_start_before', $query->query_vars['ondate']);
-		$query->set('event_end_after', $query->query_vars['ondate']);
+		$query->set('event_start_after', $ondate_start);
+		$query->set('event_end_before', $ondate_end);
 	}
 
 	//If not on event, stop here.
@@ -97,7 +122,14 @@ function eventorganiser_pre_get_posts( $query ) {
 	$date_objs = array('event_start_after'=>'','event_start_before'=>'','event_end_after'=>'','event_end_before'=>'');
 	foreach($date_objs as $prop => $value):
 		$date = $query->get($prop);
-		$date_objs[$prop] = ( empty($date) ? false : new DateTime($date, eo_get_blog_timezone()) );
+		try{
+			$date = ( empty($date) ? false : new DateTime($date, eo_get_blog_timezone()) );
+		}catch( Exception $e){
+			$date = false;
+		}
+		$date_objs[$prop] = $date;
+		$query->set($prop, $date);
+		
 	endforeach;
 
 	//If eo_interval is set, determine date ranges

commit 309127b3d511cc1f88f90fa70c9e924e2ad07314

Fixes bug in eo_get_next_occurrence introduced in 7b0724e5b0014def9af3b9c928e05449a7d38788

includes/event-organiser-event-functions.php
@@ -326,7 +326,8 @@ function eo_get_next_occurrence_of($post_id=0){
 	$post_id = (int) ( empty($post_id) ? get_the_ID() : $post_id);
 	
 	//Retrieve the blog's local time and create the date part
-	$blog_now = new DateTime(null, eo_get_blog_timezone() );
+	$tz = eo_get_blog_timezone();
+	$blog_now = new DateTime(null, $tz );
 	$now_date =$blog_now->format('Y-m-d');
 	$now_time =$blog_now->format('H:i:s');
 	