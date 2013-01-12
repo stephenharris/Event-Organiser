220af79c72313601cf20063f5ee5d523a4e17f80
Stephen Harris <contact@stephenharris.info>
2013-01-12 19:24:29 +0000

    Improves default single event page. Recurring events list dates

--- templates/event-meta-event-single.php
+++ templates/event-meta-event-single.php
@@ -31,32 +31,89 @@
 	}else{
 		$date_format = 'j F Y g:ia'; 
 	} ?>
+	<hr>
 
-	<!-- Is event recurring or a single event -->
-	<?php if( eo_reoccurs() ):?>
+	<!-- Event details -->
+		<strong><?php _e('Event Details', 'eventorganiser') ;?></strong>
 
-		<!-- Event reoccurs - is there a next occurrence? -->
-		<?php $next =   eo_get_next_occurrence($date_format);?>
+		<!-- Is event recurring or a single event -->
+		<?php if( eo_reoccurs() ):?>
+			<!-- Event reoccurs - is there a next occurrence? -->
+			<?php $next =   eo_get_next_occurrence($date_format);?>
 
-		<?php if($next): ?>
-			<!-- If the event is occurring again in the future, display the date -->
-			<?php printf('<p>'.__('This event is running from %1$s until %2$s. It is next occurring at %3$s','eventorganiser').'.</p>', eo_get_schedule_start('d F Y'), eo_get_schedule_last('d F Y'), $next);?>
+			<?php if($next): ?>
+				<!-- If the event is occurring again in the future, display the date -->
+				<?php printf('<p>'.__('This event is running from %1$s until %2$s. It is next occurring on %3$s','eventorganiser').'.</p>', eo_get_schedule_start('j F Y'), eo_get_schedule_last('j F Y'), $next);?>
 	
-		<?php else: ?>
-			<!-- Otherwise the event has finished (no more occurrences) -->
-			<?php printf('<p>'.__('This event finished on %s','eventorganiser').'.</p>', eo_get_schedule_last('d F Y',''));?>
+			<?php else: ?>
+				<!-- Otherwise the event has finished (no more occurrences) -->
+				<?php printf('<p>'.__('This event finished on %s','eventorganiser').'.</p>', eo_get_schedule_last('d F Y',''));?>
+			<?php endif; ?>
 		<?php endif; ?>
 
-	<?php else: ?>
-		<!-- Event is a single event -->
-		<?php printf('<p>'.__('This event is on %s','eventorganiser').'.</p>', eo_get_the_start($date_format) );?>
-	<?php endif; ?>
+	<ul id="eo-event-meta" style="float:left;width:40%">
 
+		<?php if( !eo_reoccurs() ){ ?>
+				<!-- Single event -->
+				<li><strong><?php _e('Start', 'eventorganiser') ;?></strong> <?php eo_the_start($date_format); ?> </li>
+				<?php
+		 } ?>
+
+		<?php if( eo_get_venue() ){ ?>
+			<li><strong><?php _e('Venue','eventorganiser'); ?></strong> <a href="<?php eo_venue_link(); ?>"> <?php eo_venue_name(); ?></a></li>
+		<?php } ?>
+
+
+		<?php if( get_the_terms(get_the_ID(),'event-category') ){ ?>
+			<li><strong><?php _e('Categories','eventorganiser'); ?></strong> <?php echo get_the_term_list( get_the_ID(),'event-category', '', ', ', '' ); ?></li>
+		<?php } ?>
+
+	
+		<?php if( get_the_terms(get_the_ID(),'event-category') ){ ?>
+			<li><strong><?php _e('Tags','eventorganiser'); ?></strong> <?php echo get_the_term_list( get_the_ID(),'event-tag', '', ', ', '' ); ?></li>
+		<?php } ?>
+
+		<?php if( eo_reoccurs() ){ 			
+				//Event reoccurs - display dates. 
+				$upcoming = new WP_Query(array(
+					'post_type'=>'event',
+					'event_start_after' => 'today',
+					'posts_per_page' => -1,
+					'event_series' => get_the_ID(),
+					'group_events_by'=>'occurrence'//Don't group by series
+				));
+	
+				if( $upcoming->have_posts() ): ?>
+
+					<li><strong><?php _e('Upcoming Dates','eventorganiser'); ?></strong>
+						<ul id="eo-upcoming-dates">
+							<?php while( $upcoming->have_posts() ): $upcoming->the_post(); ?>
+									<li> <?php eo_the_start($date_format) ?></li>
+							<?php endwhile; ?>
+						</ul>
+					</li>
+ 
+					<?php 
+					wp_reset_postdata(); 
+					//JS will hide all but the next 5 dates, with options to show more.
+					//wp_enqueue_script('eo_front');
+					?>
+				<?php endif; ?>
+		<?php } ?>
+
+	</ul>
 
 	<!-- Does the event have a venue? -->
 	<?php if( eo_get_venue() ): ?>
 		<!-- Display map -->
-		<?php echo '<p>'.eo_get_venue_map(eo_get_venue()).'</p>'; ?>
+		<div id="test" style="width:45%;float:right;">
+		<?php echo eo_get_venue_map(eo_get_venue(),array('width'=>'100%')); ?>
+		</div>
 	<?php endif; ?>
+
+
+	<div style="clear:both"></div>
+
+	<hr>
 
 </div><!-- .entry-meta -->
