stephenharris/Event-Organiser

commit 45b2355f384006263b0c08ab04190bfab0e38544

Refactors code. Adds state. Fixes #18. Optionalautofill of city & state

js/venues.js
@@ -25,9 +25,6 @@ jQuery(document).ready(function () {
             jQuery(".eo_addressInput").change(function () {
                 var address = "";
                 jQuery(".eo_addressInput").each(function () {
-                    if (jQuery(this).attr('id') != 'country-selector') {
-                        address = address + " " + jQuery(this).val()
-                    }
                 });
                 eventorganiser_code_address(address)
             })

classes/class-eo-venue-list-table.php
@@ -36,24 +36,21 @@ class EO_Venue_List_Table extends WP_List_Table {
      */
     function column_default($item, $column_name){
 		$term_id = (int) $item->term_id;
+		$address = eo_get_venue_address($term_id);
+		
 		 switch($column_name){
-			case 'venue_address':
-				$address = eo_get_venue_address($term_id);
-				return esc_html($address['address']);
-			case 'venue_postal':
-				$address = eo_get_venue_address($term_id);
-				return esc_html($address['postcode']);
-			case 'venue_city':
-				$address = eo_get_venue_address($term_id);
-				return esc_html($address['city']);
-			case 'venue_country':
-				$address = eo_get_venue_address($term_id);
-				return esc_html($address['country']);
 			case 'venue_slug':
 				return esc_html($item->slug);
 			case 'posts':
 				return intval($item->count);
 			default:
+				$address_keys = array_keys($address);
+				foreach( $address_keys as $key ){
+					if( 'venue_'.$key == $column_name ){
+						return esc_html($address[$key]);
+					}
+				}
+				//TODO Hook for extra columns?
 				return print_r($item,true); //Show the whole array for troubleshooting purposes
 		}
     }
@@ -110,7 +107,8 @@ class EO_Venue_List_Table extends WP_List_Table {
             'name'		=> array('name',true),   //true means its sorted by default  
             'venue_address'     => array('address',false),  
             'venue_city'	=> array('city',false), 
-            'venue_postal'	=> array('postcode',false),
+            'venue_state'	=> array('state',false),
+            'venue_postcode'	=> array('postcode',false),
             'venue_country'	=> array('country',false),
             'venue_slug'	=> array('slug',false),
             'posts'		=> array('count',false),

css/eventorganiser-admin-style.css
@@ -15,7 +15,7 @@
 #venue_address table tr{line-height: 4em;}
 
 /* Venue map on venue page*/
-#venue_address.postbox #venuemap{height: 250px;margin: 0 0 0 -60%;width: 60%;}
+#venue_address.postbox #venuemap{height: 300px;margin: 0 0 0 -60%;width: 60%;}
 
 /* Venue address table in metabox*/
 #eo_venue_form table.address {float:left;text-align:left;margin: 10px;}

event-organiser-edit.php
@@ -172,7 +172,6 @@ function eventorganiser_details_metabox($post){
 
 					</td>
 				</tr>
-
 				<tr valign="top"class="eo-venue-combobox-select">
 					<td class="eo-label"> <?php _e("Venue",'eventorganiser');?>: </td>
 					<td> 	
@@ -184,26 +183,26 @@ function eventorganiser_details_metabox($post){
 						</select>
 					</td>
 				</tr>
+		
+				<!-- Add New Venue --> 
 				<tr valign="top"class="eo-add-new-venue">
 					<td class="eo-label"><label><?php _e('Venue Name','eventorganiser');?>:</label></td>
 					<td><input type="text" name="eo_venue[name]" id="eo_venue_name"  value=""/></td>
 				</tr>
-				<tr valign="top"class="eo-add-new-venue">
-					<td class="eo-label"><label><?php _e('Address','eventorganiser');?>:</label></td>
-					<td><input type="text" name="eo_venue[address]" class="eo_addressInput" id="eo_venue_add"  value=""/></td>
-				</tr>
-				<tr valign="top" class="eo-add-new-venue" >
-					<td class="eo-label"><label><?php _e('City','eventorganiser');?>:</label></td>
-					<td><input type="text" name="eo_venue[city]" class="eo_addressInput" id="eo_venue_city"  value=""/></td>
-				</tr>
-				<tr valign="top" class="eo-add-new-venue" >
-					<td class="eo-label"><label><?php _e('Post Code','eventorganiser');?>:</label></td>
-					<td><input type="text" name="eo_venue[postcode]" class="eo_addressInput" id="eo_venue_pcode"  value=""/></td>
-				</tr>
-				<tr valign="top" class="eo-add-new-venue" >
-					<td class="eo-label"><label><?php _e('Country','eventorganiser');?>:</label></td>
-					<td><input type="text" name="eo_venue[country]" class="eo_addressInput" id="eo_venue_country"  value=""/></td>
-				</tr>
+			<?php
+				$address_fields = _eventorganiser_get_venue_address_fields();
+				foreach( $address_fields as $key => $label ){
+					//Keys are prefixed by '_'.
+					$key = trim($key,'_');
+					printf('<tr valign="top"class="eo-add-new-venue">
+								<th><label>%1$s:</label></th>
+								<td><input type="text" name="eo_venue[%2$s]" class="eo_addressInput" id="eo_venue_add"  value=""/></td>
+							</tr>',
+							$label,
+							esc_attr($key)
+					);
+				}
+			?>
 				<tr valign="top" class="eo-add-new-venue" >
 					<td class="eo-label"></td>
 					<td>
@@ -211,6 +210,7 @@ function eventorganiser_details_metabox($post){
 					</td>
 				</tr>
 
+				<!-- Venue Map --> 
 				<tr valign="top"  class="venue_row <?php if (!$venue_id) echo 'novenue';?>" >
 					<td class="eo-label"></td>
 					<td>

event-organiser-venues.php
@@ -336,22 +336,21 @@ function eventorganiser_venue_location($venue){
 
 	<table class ="address">
 		<tbody>
-			<tr>
-				<th><label><?php _e('Address','eventorganiser');?>:</label></th>
-				<td><input type="text" name="eo_venue[address]" class="eo_addressInput" id="eo_venue_add"  value="<?php echo esc_attr($address['address']) ;?>"/></td>
-			</tr>
-			<tr>
-				<th><label><?php _e('City','eventorganiser');?>:</label></th>
-				<td><input type="text" name="eo_venue[city]" class="eo_addressInput" id="eo_venue_city"  value="<?php echo esc_attr($address['city']) ;?>"/></td>
-			</tr>
-			<tr>
-				<th><label><?php _e('Post Code','eventorganiser');?>:</label></th>
-				<td><input type="text" name="eo_venue[postcode]" class="eo_addressInput" id="eo_venue_pcode"  value="<?php echo esc_attr($address['postcode']);?>"/></td>
-			</tr>
-			<tr>
-				<th><label><?php _e('Country','eventorganiser');?>:</label></th>
-				<td><input type="text" name="eo_venue[country]" class="eo_addressInput" id="eo_venue_country"  value="<?php echo esc_attr($address['country']) ;?>"/></td>
-			</tr>
+			<?php
+				$address_fields = _eventorganiser_get_venue_address_fields();
+				foreach( $address_fields as $key => $label ){
+					//Keys are prefixed by '_'.
+					$key = trim($key,'_');
+					printf('<tr>
+								<th><label>%1$s:</label></th>
+								<td><input type="text" name="eo_venue[%2$s]" class="eo_addressInput" id="eo_venue_add"  value="%3$s"/></td>
+							</tr>',
+							$label,
+							esc_attr($key),
+							esc_attr($address[$key]) 
+					);
+				}
+			?>
 		</tbody>
 	</table>
 
@@ -395,12 +394,15 @@ function eventorganiser_venue_admin_columns($columns){
 	$columns = array(
 		'cb'		=> '<input type="checkbox" />', //Render a checkbox instead of text
 		'name'		=> __('Venue', 'eventorganiser'),
-		'venue_address' =>__('Address', 'eventorganiser'),
-		'venue_city'	=> __('City', 'eventorganiser'),
-		'venue_postal'	=> __('Post Code', 'eventorganiser'),
-		'venue_country' => __('Country', 'eventorganiser'),
+	);
+	$address_columns = _eventorganiser_get_venue_address_fields();
+	foreach( $address_columns  as $key => $label )
+		$columns['venue'.$key] = $label;
+
+	$columns += array(
 		'venue_slug'    =>__('Slug'),
 		'posts'		=>__('Events', 'eventorganiser'),
 	);
+
 	return $columns;	
 }

includes/event-organiser-ajax.php
@@ -546,12 +546,16 @@ function eventorganiser_search_venues() {
 
 		foreach($venues as $venue){
 			$venue_id = (int) $venue->term_id;
+
 			if( !isset($term->venue_address) ){
+				/* This is all deprecated - use the API {@link http://wp-event-organiser.com/documentation/function/eo_get_venue_address/} */
 				$address = eo_get_venue_address($venue_id);
 				$venue->venue_address =  isset($address['address']) ? $address['address'] : '';
-				$venue->venue_postal =  isset($address['postcode']) ? $address['postcode'] : '';
+				$venue->venue_postal =  isset($address['postcode']) ? $address['postcode'] : ''; 
+				$venue->venue_postcode =  isset($address['postcode']) ? $address['postcode'] : '';
 				$venue->venue_city =  	isset($address['city']) ? $address['city'] : '';
 				$venue->venue_country =  isset($address['country']) ? $address['country'] : '';
+				$venue->venue_state =  isset($address['state']) ? $address['state'] : '';
 			}
 
 			if( !isset($venue->venue_lat) || !isset($venue->venue_lng) ){

includes/event-organiser-cpt.php
@@ -753,7 +753,6 @@ function eventorganiser_update_venue_meta_cache( $terms, $tax){
           	$term_ids = wp_list_pluck($terms,'term_id');
 
    		update_meta_cache('eo_venue',$term_ids);
-		$fields = array('venue_address','venue_postal','venue_city','venue_country','venue_lng','venue_lat','venue_description');
 
 		//Backwards compatible. Depreciated - use the functions, not properties.
 		foreach ($terms as $term){
@@ -763,10 +762,8 @@ function eventorganiser_update_venue_meta_cache( $terms, $tax){
 
 			if( !isset($term->venue_address) ){
 				$address = eo_get_venue_address($term_id);
-				$term->venue_address =  isset($address['address']) ? $address['address'] : '';
-				$term->venue_postal =  isset($address['postcode']) ? $address['postcode'] : '';
-				$term->venue_city =  isset($address['city']) ? $address['city'] : '';
-				$term->venue_country =  isset($address['country']) ? $address['country'] : '';
+				foreach( $address as $key => $value )
+					$term->{'venue_'.$key} = $value;
 			}
 
 			if( !isset($term->venue_lat) || !isset($term->venue_lng) ){
@@ -789,7 +786,7 @@ add_filter('get_event-venue','eventorganiser_update_venue_meta_cache',10,2);
 
 
 /**
- * Allows event-venue terms to be sorted by address, city, country, or postcode (on venue admin table)
+ * Allows event-venue terms to be sorted by address, city, state, country, or postcode (on venue admin table)
  * Hooked onto terms_clauses
  *
  * @ignore
@@ -802,16 +799,12 @@ function eventorganiser_join_venue_meta($pieces,$taxonomies,$args){
 	if( ! in_array('event-venue',$taxonomies) )
 		return $pieces;
 
-	switch($args['orderby']):
-		case 'address':
-		case 'city':
-		case 'country':
-		case 'postcode':
-			$meta_key ='_'.$args['orderby'];
-			break;
-		default:
-			$meta_key = false;
-	endswitch;
+	/* Order by */
+	$address_keys = array_keys(_eventorganiser_get_venue_address_fields());
+	if( in_array('_'.$args['orderby'], $address_keys) )
+		$meta_key ='_'.$args['orderby'];
+	else
+		$meta_key = false;
 
 	if(false === $meta_key)
 		return $pieces;

includes/event-organiser-register.php
@@ -510,7 +510,7 @@ class EO_Admin_Notice_Handler{
 		$notices = array(
 			'autofillvenue17'=>array(
 				'screen_id'=>'event_page_venues',
-				'message' => sprintf(__('A <strong>city field</strong> for venues has now been added. </br> If you like, Event Organiser can <a href="%s">attempt to auto-fill the city field</a>. You can always manually change the details aftewards.','eventorganiser'),
+				'message' => sprintf(__("<h4>City & State Fields Added</h4>City and state / province fields for venues have now been added. </br> If you'd like, Event Organiser can <a href='%s'>attempt to auto-fill them</a>. You can always manually change the details aftewards.",'eventorganiser'),
 								add_query_arg('action','eo-autofillcity',admin_url('admin-post.php'))
 								),
 				'type' => 'alert'
@@ -535,7 +535,7 @@ class EO_Admin_Notice_Handler{
 
 			$class = $notice['type'] == 'error' ? 'error' : 'updated';
 	
-			printf("<div class='%s-notice {$class}' id='%s'><p>%s </p><p> <a class='%s' href='%s' title='%s'><strong>%s</strong></a></p></div>",
+			printf("<div class='%s-notice {$class}' id='%s'>%s<p> <a class='%s' href='%s' title='%s'><strong>%s</strong></a></p></div>",
 						esc_attr(self::$prefix),
 						esc_attr(self::$prefix.'-notice-'.$id),
 						$notice['message'],
@@ -656,7 +656,7 @@ function _eventorganiser_autofill_city(){
 		$venue_id = (int) $venue->term_id;
 		$latlng =extract(eo_get_venue_latlng($venue_id));
 
-		if( eo_get_venue_meta($venue_id,'_city',true) )
+		If( eo_get_venue_meta($venue_id,'_city',true) )
 			continue;
 
 		$response=wp_remote_get("http://open.mapquestapi.com/nominatim/v1/reverse?format=json&lat={$lat}&lon={$lng}&osm_type=N&limit=1");	
@@ -664,7 +664,19 @@ function _eventorganiser_autofill_city(){
 		if( isset($geo->address->city) ){
 			$cities[$venue_id] = $geo->address->city;
 			eo_update_venue_meta($venue_id, '_city', $geo->address->city);
-		}		
+		}
+		if( isset($geo->address->country_code) && 'gb' == $geo->address->country_code ){
+			//For the UK use county not state.
+			if( isset($geo->address->county) ){
+				$cities[$venue_id] = $geo->address->county;
+				eo_update_venue_meta($venue_id, '_state', $geo->address->county);
+			}
+		}else{
+			if( isset($geo->address->state) ){
+				$cities[$venue_id] = $geo->address->state;
+				eo_update_venue_meta($venue_id, '_state', $geo->address->state);
+			}
+		}
 	}	
 
 	wp_safe_redirect(admin_url('edit.php?post_type=event&page=venues'));

commit 64d8b65402727db2277eaf249534efb5265a4bfe

Fixes geolocate bug introduced in 45b2355f38. Ref #18.

js/venues.js
@@ -23,10 +23,11 @@ jQuery(document).ready(function () {
 			}
 		}
             jQuery(".eo_addressInput").change(function () {
-                var address = "";
+                var address = [];
                 jQuery(".eo_addressInput").each(function () {
+			address.push(jQuery(this).val());
                 });
-                eventorganiser_code_address(address)
+                eventorganiser_code_address(address.join(', '))
             })
         }
     }