8ecbbcdf11aab771c82d68d0e0cbe5efe3b50294
Stephen Harris <contact@stephenharris.info>
2013-01-18 00:43:53 +0000

    Improve event meta template. Refs #13

--- templates/event-meta-event-single.php
+++ templates/event-meta-event-single.php
@@ -55,22 +55,22 @@
 
 		<?php if( !eo_reoccurs() ){ ?>
 				<!-- Single event -->
-				<li><strong><?php _e('Start', 'eventorganiser') ;?></strong> <?php eo_the_start($date_format); ?> </li>
+				<li><strong><?php _e('Start', 'eventorganiser') ;?>:</strong> <?php eo_the_start($date_format); ?> </li>
 				<?php
 		 } ?>
 
 		<?php if( eo_get_venue() ){ ?>
-			<li><strong><?php _e('Venue','eventorganiser'); ?></strong> <a href="<?php eo_venue_link(); ?>"> <?php eo_venue_name(); ?></a></li>
+			<li><strong><?php _e('Venue','eventorganiser'); ?>:</strong> <a href="<?php eo_venue_link(); ?>"> <?php eo_venue_name(); ?></a></li>
 		<?php } ?>
 
 
 		<?php if( get_the_terms(get_the_ID(),'event-category') ){ ?>
-			<li><strong><?php _e('Categories','eventorganiser'); ?></strong> <?php echo get_the_term_list( get_the_ID(),'event-category', '', ', ', '' ); ?></li>
+			<li><strong><?php _e('Categories','eventorganiser'); ?>:</strong> <?php echo get_the_term_list( get_the_ID(),'event-category', '', ', ', '' ); ?></li>
 		<?php } ?>
 
 	
 		<?php if( get_the_terms(get_the_ID(),'event-category') ){ ?>
-			<li><strong><?php _e('Tags','eventorganiser'); ?></strong> <?php echo get_the_term_list( get_the_ID(),'event-tag', '', ', ', '' ); ?></li>
+			<li><strong><?php _e('Tags','eventorganiser'); ?>:</strong> <?php echo get_the_term_list( get_the_ID(),'event-tag', '', ', ', '' ); ?></li>
 		<?php } ?>
 
 		<?php if( eo_reoccurs() ){ 			
@@ -85,7 +85,7 @@
 	
 				if( $upcoming->have_posts() ): ?>
 
-					<li><strong><?php _e('Upcoming Dates','eventorganiser'); ?></strong>
+					<li><strong><?php _e('Upcoming Dates','eventorganiser'); ?>:</strong>
 						<ul id="eo-upcoming-dates">
 							<?php while( $upcoming->have_posts() ): $upcoming->the_post(); ?>
 									<li> <?php eo_the_start($date_format) ?></li>
