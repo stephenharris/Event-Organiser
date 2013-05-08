f2cea3dd7a715f47ac425bd5e4863fa2a27f783e
Stephen Harris <contact@stephenharris.info>
2013-05-08 02:07:26 +0100

    Add eo_color_luminance utility function

--- includes/event-organiser-utility-functions.php
+++ includes/event-organiser-utility-functions.php
@@ -907,4 +907,25 @@ function _eventorganiser_enqueue_inline_help_scripts(){
 	wp_enqueue_script( 'eo-inline-help' );
 	wp_enqueue_style( 'eventorganiser-style' );
 }
+
+function eo_color_luminance( $hex, $percent ) {
+
+	// validate hex string
+
+	$hex = preg_replace( '/[^0-9a-f]/i', '', $hex );
+	$new_hex = '#';
+
+	if ( strlen( $hex ) < 6 ) {
+		$hex = $hex[0] + $hex[0] + $hex[1] + $hex[1] + $hex[2] + $hex[2];
+	}
+
+	// convert to decimal and change luminosity
+	for ($i = 0; $i < 3; $i++) {
+		$dec = hexdec( substr( $hex, $i*2, 2 ) );
+		$dec = min( max( 0, $dec + $dec * $percent ), 255 );
+		$new_hex .= str_pad( dechex( $dec ) , 2, 0, STR_PAD_LEFT );
+	}
+
+	return $new_hex;
+}
 ?>
