6cea676e41bd1a79d990b440d0fc4669574c4609
Stephen Harris <contact@stephenharris.info>
2013-05-29 23:31:44 +0100

    Add support for event_occurrence__in in calendar ajax call

--- includes/event-organiser-ajax.php
+++ includes/event-organiser-ajax.php
@@ -52,6 +52,11 @@ function eventorganiser_public_fullcalendar() {
 	if( !empty( $_GET['users_events'] ) && 'false' != $_GET['users_events'] ){
 		$request['bookee_id'] = get_current_user_id();	
 	}
+	
+	if( !empty( $_GET['event_occurrence__in'] ) ){
+		$request['event_occurrence__in'] = $_GET['event_occurrence__in'];
+	}
+	
 
 	$presets = array('numberposts'=>-1, 'group_events_by'=>'','showpastevents'=>true);
 
