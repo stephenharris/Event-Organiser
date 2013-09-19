stephenharris/Event-Organiser

commit 8b5e5c1fcaf80d3fa61adcc886e5e2341edbf2f5

Update insert/update event API

includes/event.php
@@ -35,10 +35,23 @@
 * @param array $post_data - array of data to be used by wp_update_post.
 * @return int $post_id - the post ID of the updated event
 */
-function eo_update_event($post_id, $event_data=array(), $post_data=array() ){
+function eo_update_event( $post_id, $event_data = array(), $post_data = array() ){
 
 	$post_id = (int) $post_id;
-
+	
+	$input = array_merge( $post_data, $event_data );
+	
+	$event_keys = array_flip( array( 'start', 'end', 'schedule', 'schedule_meta', 'frequency', 
+			'all_day', 'schedule_last', 'include', 'exclude', 'occurs_by') );
+	
+	$post_keys = array_flip( array(
+			'post_status', 'post_type','post_author','ping_status','post_parent','menu_order', 
+			'to_ping', 'pinged', 'post_password', 'guid', 'post_content_filtered', 'post_excerpt',
+	) );
+	
+	$event_data = array_intersect_key( $input, $event_keys );
+	$post_data = array_intersect_key( $input, $post_keys ) + $post_data;
+	 
 	if( empty($post_id) )
 		return new WP_Error('eo_error','Empty post ID.');
 
@@ -166,9 +179,22 @@ function eo_update_event($post_id, $event_data=array(), $post_data=array() ){
 * @param array $event_data array of event data
 * @return int the post ID of the updated event
 */
-function eo_insert_event($post_data=array(),$event_data=array()){
+function eo_insert_event( $post_data = array(), $event_data = array() ){
 	global $wpdb;
 
+	$input = array_merge( $post_data, $event_data );
+	
+	$event_keys = array_flip( array( 'start', 'end', 'schedule', 'schedule_meta', 'frequency', 
+			'all_day', 'schedule_last', 'include', 'exclude', 'occurs_by') );
+	
+	$post_keys = array_flip( array(
+			'post_status', 'post_type','post_author','ping_status','post_parent','menu_order', 
+			'to_ping', 'pinged', 'post_password', 'guid', 'post_content_filtered', 'post_excerpt',
+	) );
+	
+	$event_data = array_intersect_key( $input, $event_keys ) + $event_data;
+	$post_data = array_intersect_key( $input, $post_keys );
+	
 	if( !empty($event_data['venue'] ) ){
 		$post_data['tax_input']['event-venue'] = $event_data['venue'];
 		unset($event_data['venue']);

commit 51fd0d73159d6efbcef5ed5cf38cba814feed0cd

Add post title and post content to white list. Fixes bug introduced in 8b5e5c1fcaf80d3fa61adcc886e5e2341edbf2f5.

includes/event.php
@@ -45,8 +45,8 @@ function eo_update_event( $post_id, $event_data = array(), $post_data = array() 
 			'all_day', 'schedule_last', 'include', 'exclude', 'occurs_by') );
 	
 	$post_keys = array_flip( array(
-			'post_status', 'post_type','post_author','ping_status','post_parent','menu_order', 
-			'to_ping', 'pinged', 'post_password', 'guid', 'post_content_filtered', 'post_excerpt',
+			'post_title','post_content','post_status', 'post_type','post_author','ping_status','post_parent','menu_order', 
+			'to_ping', 'pinged', 'post_password', 'guid', 'post_content_filtered', 'post_excerpt', 'import_id'
 	) );
 	
 	$event_data = array_intersect_key( $input, $event_keys );
@@ -188,9 +188,10 @@ function eo_insert_event( $post_data = array(), $event_data = array() ){
 			'all_day', 'schedule_last', 'include', 'exclude', 'occurs_by') );
 	
 	$post_keys = array_flip( array(
-			'post_status', 'post_type','post_author','ping_status','post_parent','menu_order', 
-			'to_ping', 'pinged', 'post_password', 'guid', 'post_content_filtered', 'post_excerpt',
+			'post_title','post_content','post_status', 'post_type','post_author','ping_status','post_parent','menu_order', 
+			'to_ping', 'pinged', 'post_password', 'guid', 'post_content_filtered', 'post_excerpt', 'import_id'
 	) );
+	
 	
 	$event_data = array_intersect_key( $input, $event_keys ) + $event_data;
 	$post_data = array_intersect_key( $input, $post_keys );