2cf7a37d1dc033cfd9570d41e822ffe1b0be89f3
Stephen Harris <contact@stephenharris.info>
2013-12-27 11:14:28 +0000

    inline hook documentation: class/class-eo-widgets-categories.php

--- classes/class-eo-widget-categories.php
+++ classes/class-eo-widget-categories.php
@@ -43,7 +43,19 @@ class EO_Widget_Categories extends WP_Widget {
 			$cat_args['walker'] = new EO_Walker_TaxonomyDropdown();
 			$cat_args['value'] = 'slug';
 			$cat_args['show_option_none'] = __( 'Select Category' );
-			wp_dropdown_categories(apply_filters('eventorganiser_widget_event_categories_dropdown_args', $cat_args));
+			/**
+			 * Filters the settings for the event category dropdown.
+			 * 
+			 * The filtered array is passed to `wp_dropdown_categories()`. See
+			 * the [WordPress codex](http://codex.wordpress.org/Function_Reference/wp_dropdown_categories Codex) 
+			 * for details on its arguments.
+			 * 
+			 * @package widgets
+			 * @link http://codex.wordpress.org/Function_Reference/wp_dropdown_categories Codex for `wp_dropdown_categories()`
+			 * @param array $cat_args Settings for the event category dropdown.
+			 */
+			$cat_args = apply_filters( 'eventorganiser_widget_event_categories_dropdown_args', $cat_args );
+			wp_dropdown_categories( $cat_args );
 			?>
 
 <script type='text/javascript'>
@@ -64,7 +76,19 @@ class EO_Widget_Categories extends WP_Widget {
 		<ul>
 <?php
 		$cat_args['title_li'] = '';
-		wp_list_categories(apply_filters('eventorganiser_widget_event_categories_args', $cat_args));
+		/**
+		 * Filters the arguments for the event category list.
+		 *
+		 * The filtered array is passed to `wp_list_categories()`. See
+		 * the [WordPress codex](http://codex.wordpress.org/Function_Reference/wp_list_categories Codex)
+		 * for details on its arguments.
+		 *
+		 * @package widgets
+		 * @link http://codex.wordpress.org/Function_Reference/wp_list_categories Codex for `wp_list_categories()`
+		 * @param array $cat_args Settings for the event category list.
+		 */
+		$cat_args = apply_filters('eventorganiser_widget_event_categories_args', $cat_args );
+		wp_list_categories( $cat_args );
 ?>
 		</ul>
 <?php
