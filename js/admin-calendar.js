8fd3faba5873e47122fe8cc18564631f09426a8e
Stephen Harris <contact@stephenharris.info>
2014-08-18 13:54:27 +0100

    Hide venue/category filter when there are no events/categories

--- js/admin-calendar.js
+++ js/admin-calendar.js
@@ -337,6 +337,10 @@ eventorganiser.versionCompare = function(left, right) {
 	function eventorganiser_cat_dropdown(options){
 
 		var terms = options.categories;
+		
+		if( !terms ){
+			return;
+		}
 
 		var html="<select class='eo-cal-filter' id='eo-event-cat'>";
 		html+="<option value=''>"+options.buttonText.cat+"</option>";
@@ -351,6 +355,10 @@ eventorganiser.versionCompare = function(left, right) {
 	function eventorganiser_venue_dropdown(options){
 
 		var venues = options.venues;
+		
+		if( !venues ){
+			return;
+		}
 
 		var html="<select class='eo-cal-filter' id='eo-event-venue'>";
 		html+="<option value=''>"+options.buttonText.venue+"</option>";
