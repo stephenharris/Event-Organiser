b909c8efc7064543d6fe9145c5f16f9ed0aeedc6
Stephen Harris <contact@stephenharris.info>
2014-12-20 13:16:39 +0000

    Refactor tests bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,14 @@ echo "Welcome to the Event Organiser Test Suite" . PHP_EOL;
 echo "Version: 1.0" . PHP_EOL;
 echo "Authors: Stephen Harris" . PHP_EOL;
 
+// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress developer repo under wp-content/plugins/
+$_wp_dev_dir = getenv('WP_DEVELOP_DIR');
+if ( !$_wp_dev_dir ) $_wp_dev_dir = '../../../..';
+
+echo "Using WordPress installation at ". $_wp_dev_dir . PHP_EOL;
+
 //Defines the data location for unit-tests
 define( 'EO_DIR_TESTDATA', dirname( __FILE__ ) . '/data' );
 
@@ -14,14 +22,8 @@ $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'event-organiser/event-organiser.php' ),
 );
 
-// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
-// location. Otherwise, we'll just assume that this plugin is installed in a
-// WordPress developer repo under wp-content/plugins/
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
-}
+require_once $_wp_dev_dir . '/tests/phpunit/includes/functions.php';
+require_once $_wp_dev_dir . '/tests/phpunit/includes/bootstrap.php';
 
 // Install Event Organiser
 echo "Installing Event Organiser...\n";
