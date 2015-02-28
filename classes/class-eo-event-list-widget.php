23bb23902f5c5412cd03d73720edf619c03b7932
Stephen Harris <contact@stephenharris.info>
2015-02-28 16:27:35 +0000

    Replace 'showpastevents' checkbox with 'scopes'. Allow custom scopes to be registered

--- classes/class-eo-event-list-widget.php
+++ classes/class-eo-event-list-widget.php
@@ -4,23 +4,14 @@
  */
 class EO_Event_List_Widget extends WP_Widget{
 
-	var $w_arg = array(
-		'title'=> 'Events',
-		'numberposts'=> 5,
-		'event-category'=> '',
-		'venue_id'=> NULL,
-		'venue'=> '',
-		'orderby'=> 'eventstart',
-		'showpastevents'=> 0,
-		'group_events_by'=>'',
-		'order'=> 'ASC',
-		'template'=>'',
-		'no_events'=>'No Events'
-		);
-
+	/*
+	 * Array of default settings
+	 */
+	public static $w_arg;
+	
 	function __construct() {
-		$widget_ops = array('classname' => 'EO_Event_List_Widget', 'description' => __('Displays a list of events','eventorganiser') );
-		parent::__construct('EO_Event_List_Widget', __('Events','eventorganiser'), $widget_ops);
+		$widget_ops = array( 'classname' => 'EO_Event_List_Widget', 'description' => __( 'Displays a list of events', 'eventorganiser' ) );
+		parent::__construct( 'EO_Event_List_Widget', __( 'Events','eventorganiser' ), $widget_ops );
 	}
 	
 	/**
@@ -30,126 +21,219 @@ class EO_Event_List_Widget extends WP_Widget{
 	 */
 	public static function register() {
 		register_widget( __CLASS__ );
+		
+		self::$w_arg = array(
+			'title'           => __( 'Events', 'eventorganiser' ),
+			'numberposts'     => 5,
+			'event-category'  => '',
+			'venue_id'        => null,
+			'venue'           => '',
+			'orderby'         => 'eventstart',
+			'scope'        => 'future',
+			'group_events_by' => '',
+			'order'           => 'ASC',
+			'template'        => '',
+			'no_events'       => __( 'No Events', 'eventorganiser' )
+		);
+		
 	}
 
- 
-  function form($instance){	
-	$instance = wp_parse_args( (array) $instance, $this->w_arg );
-  ?>
-<p>
-	<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'eventorganiser'); ?>: </label>
-	<input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo esc_attr($instance['title']); ?>" />
-</p>
-  <p>
-  <label for="<?php echo $this->get_field_id('numberposts'); ?>"><?php _e('Number of events','eventorganiser');?>:   </label>
-	  <input id="<?php echo $this->get_field_id('numberposts'); ?>" name="<?php echo $this->get_field_name('numberposts'); ?>" type="number" size="3" value="<?php echo intval($instance['numberposts']);?>" />
-</p>
-  <p>
-  <label for="<?php echo $this->get_field_id('event-category'); ?>"><?php _e('Event categories', 'eventorganiser'); ?>:   </label>
-  <input  id="<?php echo $this->get_field_id('event-category'); ?>" class="widefat" name="<?php echo $this->get_field_name('event-category'); ?>" type="text" value="<?php echo esc_attr($instance['event-category']);?>" />
-   <em><?php _e('List category slug(s), seperate by comma. Leave blank for all', 'eventorganiser'); ?> </em>
-</p>
-<?php 
-	if( taxonomy_exists( 'event-venue' ) ): ?>
+	function get_event_intervals(){
+		
+		$intervals = array(
+			'future' => array(
+				'label' => __( 'Future events', 'eventorganiser' ),
+				'query'	=> array(
+					'event_start_after' => 'now',
+				),
+			),	
+			'all' => array(
+				'label' => __( 'All events', 'eventorganiser' ),
+				'query'	=> array(
+					'showpastevents' => 'all',
+				),
+			),
+			'running' => array(
+				'label' => __( 'Running events', 'eventorganiser' ),
+				'query'	=> array(
+					'event_start_before' => 'now',
+					'event_end_after'    => 'now',
+				),
+			),
+			'past' => array(
+				'label' => __( 'Past events', 'eventorganiser' ),
+				'query'	=> array(
+					'event_end_before' => 'now',
+				),
+			),
+			'future-running' => array(
+				'label' => __( 'Furture & running events', 'eventorganiser' ),
+				'query'	=> array(
+					'event_end_before' => 'now',
+				),
+			),
+			'today' => array(
+				'label' => __( 'Events on today', 'eventorganiser' ),
+				'query'	=> array(
+					'event_start_before' => 'now',
+					'event_end_after'    => 'now',
+				),
+			),
+				
+		);
+		
+		$intervals = apply_filters( 'eventorganiser_query_scope', $intervals );
+		
+		return $intervals;
+		
+	}
+	
+	function form($instance){	
+		$instance = wp_parse_args( (array) $instance, self::$w_arg );
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'eventorganiser' ); ?>: </label>
+			<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $instance['title'] ); ?>" />	
+		</p>
   		<p>
-	  		<label for="<?php echo $this->get_field_id('venue'); ?>"><?php _e('Venue', 'eventorganiser'); ?>:   </label>
-			<?php 	$venues = get_terms('event-venue', array('hide_empty'=>false));?>
-			<select id="<?php echo $this->get_field_id('venue'); ?>" name="<?php echo $this->get_field_name('venue'); ?>" type="text">
-				<option value="" <?php selected($instance['venue'], ''); ?>><?php _e('All Venues','eventorganiser'); ?> </option>
-				<?php foreach ($venues as $venue):?>
-					<option <?php  selected($instance['venue'],$venue->slug);?> value="<?php echo esc_attr($venue->slug);?>"><?php echo esc_html($venue->name); ?></option>
-				<?php endforeach;?>
+  			<label for="<?php echo $this->get_field_id( 'numberposts' ); ?>"><?php _e( 'Number of events', 'eventorganiser' );?>:   </label>
+	  		<input id="<?php echo $this->get_field_id( 'numberposts' ); ?>" name="<?php echo $this->get_field_name( 'numberposts' ); ?>" type="number" size="3" value="<?php echo intval( $instance['numberposts'] );?>" />
+		</p>
+  		<p>
+  			<label for="<?php echo $this->get_field_id( 'event-category' ); ?>"><?php _e( 'Event categories', 'eventorganiser' ); ?>:   </label>
+	  		<input  id="<?php echo $this->get_field_id( 'event-category' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'event-category' ); ?>" type="text" value="<?php echo esc_attr( $instance['event-category'] );?>" />
+   			<em><?php _e( 'List category slug(s), seperate by comma. Leave blank for all', 'eventorganiser' ); ?></em>
+		</p>
+		<?php 
+		if( taxonomy_exists( 'event-venue' ) ){ ?>
+  			<p>
+	  			<label for="<?php echo $this->get_field_id( 'venue' ); ?>"><?php _e( 'Venue:', 'eventorganiser' ); ?></label>
+				<?php $venues = get_terms( 'event-venue', array( 'hide_empty' => false ) );?>
+				<select id="<?php echo $this->get_field_id( 'venue' ); ?>" name="<?php echo $this->get_field_name( 'venue' ); ?>" type="text">
+					<option value="" <?php selected( $instance['venue'], '' ); ?>><?php _e( 'All Venues', 'eventorganiser' ); ?> </option>
+					<?php foreach( $venues as $venue ):?>
+						<option <?php selected( $instance['venue'], $venue->slug );?> value="<?php echo esc_attr( $venue->slug );?>"><?php echo esc_html( $venue->name ); ?></option>
+					<?php endforeach;?>
+				</select>
+			</p>
+		<?php } ?>
+		<p>
+  			<label for="<?php echo $this->get_field_id( 'scope' ); ?>"><?php _e( 'Show:', 'eventorganiser' ); ?></label>
+			<select id="<?php echo $this->get_field_id( 'scope' ); ?>" name="<?php echo $this->get_field_name( 'scope' ); ?>" type="text">
+				<?php 
+				foreach( $this->get_event_intervals() as $scope_id => $scope ){ 
+					printf( 
+						'<option value="%s" %s>%s</option>', 
+						$scope_id,
+						selected( $instance['scope'], $scope_id, false ), 
+						$scope['label'] 
+					);
+				}
+				?>
 			</select>
 		</p>
-	<?php endif; ?>
-  <p>
-  <label for="<?php echo $this->get_field_id('orderby'); ?>"><?php _e('Order by', 'eventorganiser'); ?></label>
-	<select id="<?php echo $this->get_field_id('orderby'); ?>" name="<?php echo $this->get_field_name('orderby'); ?>" type="text">
-		<option value="eventstart" <?php selected($instance['orderby'], 'eventstart'); ?>><?php _e('Start date', 'eventorganiser'); ?></option>
-		<option value="title" <?php selected($instance['orderby'], 'title');?>><?php _e('Title', 'eventorganiser'); ?> </option>
-		<option value="date" <?php selected($instance['orderby'], 'date');?>><?php _e('Publish date', 'eventorganiser'); ?> </option>
-	</select>
-	<select id="<?php echo $this->get_field_id('order'); ?>" name="<?php echo $this->get_field_name('order'); ?>" type="text">
-		<option value="asc" <?php selected($instance['order'], 'asc'); ?>><?php _e('ASC', 'eventorganiser'); ?> </option>
-		<option value="desc" <?php selected($instance['order'], 'desc');?>><?php _e('DESC', 'eventorganiser'); ?> </option>
-	</select>
-</p>
-  <p>
-    <label for="<?php echo $this->get_field_id('showpastevents'); ?>"><?php _e('Include past events', 'eventorganiser'); ?>  </label>
-	<input type="checkbox" id="<?php echo $this->get_field_id('showpastevents'); ?>" name="<?php echo $this->get_field_name('showpastevents'); ?>" <?php checked($instance['showpastevents'],1);?> value="1" />
-</p>
-  <p>
-    <label for="<?php echo $this->get_field_id('group_events_by'); ?>"><?php _e('Group occurrences', 'eventorganiser'); ?>  </label>
-	<input type="checkbox" id="<?php echo $this->get_field_id('group_events_by'); ?>" value="series" name="<?php echo $this->get_field_name('group_events_by'); ?>" <?php checked($instance['group_events_by'],'series');?> />
-  </p>
-  <p>
-    <label for="<?php echo $this->get_field_id('template'); ?>">
-	<?php 
-		_e('Template (leave blank for default)', 'eventorganiser'); 
-		echo eventorganiser_inline_help( 
-			__( 'Event list widget placeholders', 'eventorganiser' ),
-			sprintf(
-					__( 'You can use specified tags as placeholders for event information which you want to appear in the widget. <a href="%s" target="_blank"> Find out more</a>.', 'eventorganiser' ),
-					'http://docs.wp-event-organiser.com/widgets/events-list'
-				)
-			);
-	 ?>
-	</label>
-	  <input  id="<?php echo $this->get_field_id('template'); ?>" class="widefat" name="<?php echo $this->get_field_name('template'); ?>" type="text" value="<?php echo esc_attr($instance['template']);?>" />
+		      
+		<p>
+  			<label for="<?php echo $this->get_field_id( 'orderby' ); ?>"><?php _e( 'Order by', 'eventorganiser' ); ?></label>
+			<select id="<?php echo $this->get_field_id( 'orderby' ); ?>" name="<?php echo $this->get_field_name( 'orderby' ); ?>">
+				<option value="eventstart" <?php selected( $instance['orderby' ], 'eventstart' ); ?>><?php _e( 'Start date', 'eventorganiser' ); ?></option>
+				<option value="title" <?php selected( $instance[ 'orderby' ], 'title' );?>><?php _e( 'Title', 'eventorganiser' ); ?></option>
+				<option value="date" <?php selected( $instance[ 'orderby' ], 'date' );?>><?php _e( 'Publish date', 'eventorganiser' ); ?></option>
+			</select>
+			<select id="<?php echo $this->get_field_id( 'order' ); ?>" name="<?php echo $this->get_field_name( 'order' ); ?>" type="text">
+				<option value="asc" <?php selected( $instance['order'], 'asc' ); ?>><?php _e( 'ASC', 'eventorganiser' ); ?> </option>
+				<option value="desc" <?php selected( $instance['order'], 'desc' );?>><?php _e( 'DESC', 'eventorganiser' ); ?> </option>
+			</select>
+		</p>
 
-  </p>
-  <p>
-    <label for="<?php echo $this->get_field_id('no_events'); ?>"><?php _e("'No events' message", 'eventorganiser'); ?>  </label>
-	  <input  id="<?php echo $this->get_field_id('no_events'); ?>" class="widefat" name="<?php echo $this->get_field_name('no_events'); ?>" type="text" value="<?php echo esc_attr($instance['no_events']);?>" />
-  </p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'group_events_by' ); ?>"><?php _e( 'Group occurrences', 'eventorganiser' ); ?></label>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'group_events_by' ); ?>" value="series" name="<?php echo $this->get_field_name( 'group_events_by' ); ?>" <?php checked( $instance['group_events_by'],'series' );?> />
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'template' ); ?>">
+			<?php 
+				_e( 'Template (leave blank for default)', 'eventorganiser' ); 
+				echo eventorganiser_inline_help( 
+					__( 'Event list widget placeholders', 'eventorganiser' ),
+					sprintf(
+						__( 'You can use specified tags as placeholders for event information which you want to appear in the widget. <a href="%s" target="_blank"> Find out more</a>.', 'eventorganiser' ),
+						'http://docs.wp-event-organiser.com/widgets/events-list'
+					)
+				);
+			?>
+			</label>
+	  		<input  id="<?php echo $this->get_field_id( 'template' ); ?>" class="widefat" name="<?php echo $this->get_field_name('template'); ?>" type="text" value="<?php echo esc_attr( $instance['template'] );?>" />
+  		</p>
+  
+  		<p>
+    		<label for="<?php echo $this->get_field_id( 'no_events' ); ?>"><?php _e( "'No events' message", 'eventorganiser' ); ?>  </label>
+	  		<input  id="<?php echo $this->get_field_id( 'no_events' ); ?>" class="widefat" name="<?php echo $this->get_field_name('no_events'); ?>" type="text" value="<?php echo esc_attr( $instance['no_events'] );?>" />
+  		</p>
+		<?php
+	}
 
-<?php
-  }
- 
-  function update($new_instance, $old_instance){  
-	$validated=array();
-	$validated['title'] = sanitize_text_field( $new_instance['title'] );
-	$validated['numberposts'] = intval($new_instance['numberposts']);
-	$event_cats = array_map('sanitize_text_field', explode(',',$new_instance['event-category']));
-	$validated['event-category'] = implode(',',$event_cats);
-	$validated['venue'] = sanitize_text_field( $new_instance['venue'] );
-	$validated['order'] = ($new_instance['order'] == 'asc' ? 'asc' : 'desc');
-	$validated['orderby'] = in_array( $new_instance['orderby'],  array( 'title', 'eventstart', 'date' ) ) ? $new_instance['orderby'] : 'eventstart';
-	$validated['showpastevents'] = ( !empty($new_instance['showpastevents']) ? 1:  0);
-	$validated['group_events_by'] = ( isset($new_instance['group_events_by']) && $new_instance['group_events_by']=='series' ? 'series':  '');
-	$validated['template'] = $new_instance['template'];
-	$validated['no_events'] = $new_instance['no_events'];
-	return $validated;
-    }
+	function update( $new_inst, $old_inst ){
 
- 
-  function widget($args, $instance){
+		$intervals = array_keys( $this->get_event_intervals() );
+		
+		$validated = array(
+			'title'           => sanitize_text_field( $new_inst['title'] ),
+			'numberposts'     => intval( $new_inst['numberposts'] ),
+			'venue'           => sanitize_text_field( $new_inst['venue'] ),
+			'scope'           => isset( $new_inst['scope'] ) && in_array( $new_inst['scope'], $intervals ) ? $new_inst['scope'] : 'future', 
+			'order'           => $new_inst['order'] == 'asc' ? 'asc' : 'desc',
+			'orderby'         => in_array( $new_inst['orderby'],  array( 'title', 'eventstart', 'date' ) ) ? $new_inst['orderby'] : 'eventstart',
+			'group_events_by' => isset( $new_inst['group_events_by'] ) && $new_inst['group_events_by'] == 'series' ? 'series' : '',
+			'template'        => $new_inst['template'],
+			'no_events'       => $new_inst['no_events'],
+		);
 	
-  	$instance = array_merge( array(
-  		'no_events' => '',
-  		'template'  => '',
-  	), $instance );
-  	
-  	extract($args, EXTR_SKIP);
-  	
-  	$template = $instance['template'];
-  	$no_events = $instance['no_events'];
-	unset( $instance['template'] );
-	unset( $instance['no_events'] );
-
-    echo $before_widget;
-
-	$widget_title = apply_filters('widget_title', $instance['title'], $instance, $this->id_base);
-
-    if ( $widget_title ){
-   		echo $before_title.esc_html($widget_title).$after_title;
+		$event_cats = array_map( 'sanitize_text_field', explode( ',', $new_inst['event-category'] ) );
+		$validated['event-category'] = implode( ',', $event_cats );
+	
+		return $validated;
     }
 
-	eventorganiser_list_events( $instance, array( 'type'=>'widget','class'=>'eo-events eo-events-widget','template'=>$template, 'no_events'=>$no_events ) );
+	function widget( $args, $instance ){
+		$instance = array_merge( array(
+  			'no_events' => '',
+  			'template'  => '',
+  		), $instance );
+		
+		//Backwards compatability with show past events option
+		if( !isset( $instance['scope'] ) && isset( $instance['showpastevents'] ) ){
+			$instance['scope'] = $instance['showpastevents'] ? 'all' : 'future';
+		}
+		unset( $instance['showpastevents'] );
+  	
+  		$template  = $instance['template'];
+  		$no_events = $instance['no_events'];
+		unset( $instance['template'] );
+		unset( $instance['no_events'] );
 
-	echo $after_widget;
-     
-  }
+    	echo $args['before_widget'];
+
+		$widget_title = apply_filters('widget_title', $instance['title'], $instance, $this->id_base);
+
+    	if ( $widget_title ){
+   			echo $args['before_title'] . esc_html( $widget_title ) . $args['after_title'];
+    	}
+    	
+    	$scope     = $instance['scope'];
+    	$intervals = $this->get_event_intervals();
+    	$instance  = array_merge( $instance, (array) $intervals[$scope]['query'] );
+
+		eventorganiser_list_events( $instance, array( 
+			'type'      => 'widget',
+			'class'     => 'eo-events eo-events-widget',
+			'template'  => $template, 
+			'no_events' => $no_events 
+		) );
+
+		echo $args['after_widget'];
+	}
  
 }
 
@@ -233,8 +317,9 @@ function eventorganiser_list_events( $query, $args=array(), $echo=1 ){
 
 	wp_reset_postdata();
 
-	if( $echo )
+	if( $echo ){
 		echo $html;
+	}
 
 	return $html;
 }
