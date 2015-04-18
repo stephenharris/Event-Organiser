73d3c65a31c48f4111bda36cdaf15f93481e4a10
Stephen Harris <contact@stephenharris.info>
2015-04-18 22:17:11 +0100

    FIx warnings/ notices. $post_terms not defined.

--- event-organiser-manage.php
+++ event-organiser-manage.php
@@ -68,8 +68,9 @@ function eventorganiser_event_fill_columns( $column_name, $id ) {
 	$series_id = ( empty( $post->event_id) ? $id :'' );
 
 	$phpFormat = 'M, j Y';
-	if ( !eo_is_all_day( $series_id ) )
+	if ( !eo_is_all_day( $series_id ) ){
 		$phpFormat .= '\<\/\b\r\>'. get_option( 'time_format' );
+	}
 	
 	switch ( $column_name ) {
 		case 'venue':
@@ -95,12 +96,14 @@ function eventorganiser_event_fill_columns( $column_name, $id ) {
 			break;
 
 		case 'eventcategories':
-		    	$terms = get_the_terms( $post->ID, 'event-category' );
- 			
+			$terms = get_the_terms( $post->ID, 'event-category' );
+
 			if ( !empty( $terms) ) {
-       	 		foreach ( $terms as $term )
-			            $post_terms[] = '<a href="'.add_query_arg( 'event-category', $term->slug ).'">'.esc_html( sanitize_term_field( 'name', $term->name, $term->term_id, 'event-category', 'display' ) ).'</a>';
-			        echo join( ', ', $post_terms );
+				$post_terms = array();
+				foreach ( $terms as $term ){
+					$post_terms[] = '<a href="'.add_query_arg( 'event-category', $term->slug ).'">'.esc_html( sanitize_term_field( 'name', $term->name, $term->term_id, 'event-category', 'display' ) ).'</a>';
+				}
+				echo join( ', ', $post_terms );
 			}
 			break;
 
