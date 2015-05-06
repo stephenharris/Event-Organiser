93c34de14637243ed54c1f36a6fb9a60a074cbfd
Stephen Harris <contact@stephenharris.info>
2015-05-06 23:42:21 +0100

    Don't perform operations on end date - clone it first. Fix bug where use of %event_duration% tag modifies the end date. Also fixes faulty logic in using an alternative if date_diff doesn't exist.

--- classes/class-eventorganiser-shortcodes.php
+++ classes/class-eventorganiser-shortcodes.php
@@ -306,17 +306,19 @@ class EventOrganiser_Shortcodes {
 
 				break;
 			case 'event_duration':
-				$start = eo_get_the_start(DATETIMEOBJ);
-				$end = eo_get_the_end(DATETIMEOBJ);
-				if( eo_is_all_day() )
-					$end->modify('+1 minute');
-
-				if( !function_exists('date_diff') ){
-					$duration = date_diff($start,$end);
-					$replacement = $duration->format($matches[2]);
-				}else{
-					$replacement = eo_date_interval($start,$end,$matches[2]);
+				$start = eo_get_the_start( DATETIMEOBJ );
+				$end   = clone eo_get_the_end( DATETIMEOBJ );
+				if( eo_is_all_day() ){
+					$end->modify( '+1 minute' );
 				}
+
+				if( function_exists( 'date_diff' ) ){
+					$duration = date_diff( $start, $end );
+					$replacement = $duration->format( $matches[2] );
+				}else{
+					$replacement = eo_date_interval( $start,$end, $matches[2] );
+				}
+				$replacement = false;
 				break;
 
 			case 'event_tags':
