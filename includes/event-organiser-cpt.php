a20c65dd97aacbd7a4fff1c348a3feecc9ad079a
Stephen Harris <contact@stephenharris.info>
2015-12-05 14:56:57 +0000

    Refactor $message - avoids duplication of core strings

--- includes/event-organiser-cpt.php
+++ includes/event-organiser-cpt.php
@@ -313,26 +313,37 @@ add_action( 'init', 'eventorganiser_cpt_register' );
 function eventorganiser_messages( $messages ) {
 	global $post, $post_ID;
 
+	$permalink = get_permalink( $post_ID );
+
+	//Inherit post messages
 	$messages['event'] = array(
-    		0 => '', // Unused. Messages start at index 1.
-		1 => sprintf( __('Event updated. <a href="%s">View event</a>','eventorganiser'), esc_url( get_permalink($post_ID) ) ),
-		2 => __('Custom field updated.'),
-		3 => __('Custom field deleted.'),
-		4 => __('Event updated.','eventorganiser'),
+		1 => sprintf( __( 'Event updated. <a href="%s">View event</a>', 'eventorganiser' ), esc_url( $permalink ) ),
+		4 => __( 'Event updated.', 'eventorganiser' ),
 		/* translators: %s: date and time of the revision */
-		5 => isset($_GET['revision']) ? sprintf( __('Event restored to revision from %s','eventorganiser'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-		6 => sprintf( __('Event published. <a href="%s">View event</a>','eventorganiser'), esc_url( get_permalink($post_ID) ) ),
-		7 => __('Event saved.'),
-		8 => sprintf( __('Event submitted. <a target="_blank" href="%s">Preview event</a>','eventorganiser'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
-		9 => sprintf( __('Event scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview event</a>','eventorganiser'),
-		 // translators: Publish box date format, see https://php.net/date
-      		date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
-		10 => sprintf( __('Event draft updated. <a target="_blank" href="%s">Preview event</a>','eventorganiser'), esc_url( add_query_arg( 'preview', 'true', get_permalink($post_ID) ) ) ),
-		20 => __('This event has been broken from a recurring event.','eventorganiser')
-  	);
+		5 => isset( $_GET['revision'] ) ? sprintf( __( 'Event restored to revision from %s', 'eventorganiser' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		6 => sprintf( __( 'Event published. <a href="%s">View event</a>', 'eventorganiser' ), esc_url( $permalink ) ),
+		7 => __( 'Event saved.', 'eventorganiser' ),
+		8 => sprintf(
+			__( 'Event submitted. <a target="_blank" href="%s">Preview event</a>', 'eventorganiser' ),
+			esc_url( add_query_arg( 'preview', 'true', $permalink ) )
+		),
+		9 => sprintf(
+			__( 'Event scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview event</a>', 'eventorganiser' ),
+			// translators: Publish box date format, see https://php.net/date
+			date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( $permalink )
+		),
+		10 => sprintf(
+			__( 'Event draft updated. <a target="_blank" href="%s">Preview event</a>', 'eventorganiser' ),
+			esc_url( add_query_arg( 'preview', 'true', $permalink ) )
+		),
+		20 => __( 'This event has been broken from a recurring event.', 'eventorganiser' ),
+	) + $messages['post'];
+
+	ksort( $messages['event'] );
+
 	return $messages;
 }
-add_filter('post_updated_messages', 'eventorganiser_messages');
+add_filter( 'post_updated_messages', 'eventorganiser_messages' );
 
 
 /**
