66a2cc92e10f9a537b71dd340674b4a20ec7d7b9
Stephen Harris <contact@stephenharris.info>
2015-12-27 15:40:56 +0000

    Over-ride fill field function with a spin - remove waiting step.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -454,4 +454,45 @@ class FeatureContext extends WordPressContext implements Context, SnippetAccepti
 		update_option( 'sidebars_widgets', $active_widgets );
 		
 	}
+	
+	/**
+	 * Fills in form field with specified id|name|label|value.
+	 *
+	 * @overide When /^(?:|I )fill in "(?P<field>(?:[^"]|\\")*)" with "(?P<value>(?:[^"]|\\")*)"$/
+	 * @overide When /^(?:|I )fill in "(?P<field>(?:[^"]|\\")*)" with:$/
+	 * @overide When /^(?:|I )fill in "(?P<value>(?:[^"]|\\")*)" for "(?P<field>(?:[^"]|\\")*)"$/
+	 */
+	public function fillField($field, $value)
+	{
+		$field = $this->fixStepArgument($field);
+		$value = $this->fixStepArgument($value);
+		
+		$this->spin(function($context) use ($field, $value) {
+			$context->getSession()->getPage()->fillField($field, $value);
+			return true;
+		});
+	}
+	
+	public function spin ($lambda, $wait = 60)
+	{
+		for ($i = 0; $i < $wait; $i++)
+		{
+		try {
+			if ($lambda($this)) {
+			return true;
+			}
+			} catch (Exception $e) {
+			// do nothing
+			}
+	
+			sleep(1);
+		}
+	
+		$backtrace = debug_backtrace();
+	
+			throw new Exception(
+			"Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
+        $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
+	        );
+	}
 }
