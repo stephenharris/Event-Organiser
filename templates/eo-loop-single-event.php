f13479bda82aadd1b494a5403251f3506b9b1afb
Stephen Harris <contact@stephenharris.info>
2016-01-21 01:41:31 +0000

    Event title text should be h2 not h1, as h1 is likely to be used by the page title

--- templates/eo-loop-single-event.php
+++ templates/eo-loop-single-event.php
@@ -20,11 +20,11 @@
 					
 	<header class="eo-event-header entry-header">
 
-		<h1 class="eo-event-title entry-title">
+		<h2 class="eo-event-title entry-title">
 			<a href="<?php the_permalink(); ?>" itemprop="url">
 				<span itemprop="summary"><?php the_title() ?></span>
 			</a>
-		</h1>
+		</h2>
 
 		<div class="eo-event-date"> 
 			<?php
