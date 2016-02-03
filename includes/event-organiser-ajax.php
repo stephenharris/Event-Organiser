83549c9fb7cf5606ecdc32099ff08a84267bb39b
Stephen Harris <contact@stephenharris.info>
2016-02-03 20:51:02 +0000

    Move style attribute to stylesheet and remove title attribute. Ref #334 and Ref #323. Includes changes to adhere to coding standard.

--- includes/event-organiser-ajax.php
+++ includes/event-organiser-ajax.php
@@ -448,12 +448,12 @@ function eventorganiser_admin_calendar() {
 					$delete_url  = wp_nonce_url( $delete_url , 'eventorganiser_delete_occurrence_'.$post->occurrence_id );
 
 					$summary .= sprintf(
-						'<span class="delete"><a class="submitdelete" style="color:red;float:right" title="%1$s" href="%2$s">%1$s</a></span>',
+						'<span class="delete"><a class="submitdelete" href="%2$s">%1$s</a></span>',
 						esc_attr__( 'Delete this occurrence', 'eventorganiser' ),
 						$delete_url
 					);
 
-					if( $schedule['schedule'] != 'once' ){
+					if ( 'once' != $schedule['schedule'] ) {
 						$break_url = add_query_arg(array(
 							'post_type' => 'event',
 							'page'      => 'calendar',
@@ -461,11 +461,11 @@ function eventorganiser_admin_calendar() {
 							'event'     => $post->occurrence_id,
 							'action'    => 'break_series',
 						),$admin_url);
-						
+
 						$break_url  = wp_nonce_url( $break_url, 'eventorganiser_break_series_'.$post->occurrence_id );
 
 						$summary .= sprintf(
-							'<span class="break"><a class="submitbreak" style="color:red;float:right;padding-right:2em;" title="%1$s" href="%2$s">%1$s</a></span>',
+							'<span class="break"><a class="submitbreak" href="%2$s">%1$s</a></span>',
 							esc_attr__( 'Break this series', 'eventorganiser' ),
 							$break_url
 						);
