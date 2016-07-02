2e5c176c667e6404f646652fa7652489f38b02a4
Stephen Harris <contact@stephenharris.info>
2016-07-02 21:17:01 +0100

    Fix tests

--- tests/unit-tests/subscribeLinkTest.php
+++ tests/unit-tests/subscribeLinkTest.php
@@ -2,7 +2,7 @@
 
 class subscribeLinkTest extends EO_UnitTestCase
 {
-	
+
 	public function testWebcal()
 	{
 		$actual   = do_shortcode( '[eo_subscribe type="webcal"]Subscribe[/eo_subscribe]' );
@@ -14,29 +14,29 @@ class subscribeLinkTest extends EO_UnitTestCase
 	{
 		$value = isset( $_SERVER['HTTPS'] ) ? $_SERVER['HTTPS'] : null;
 		$_SERVER['HTTPS'] = 1;
-		
+
 		$actual   = do_shortcode( '[eo_subscribe type="webcal"]Subscribe[/eo_subscribe]' );
 		$expected = '<a href="webcal://example.org/?feed=eo-events" target="_blank"  title="Subscribe to calendar"  >Subscribe</a>';
 		$this->assertEquals( $expected, $actual );
-		
+
 		$_SERVER['HTTPS'] = $value;
 		if ( is_null( $value ) ) {
 			unset( $_SERVER['HTTPS'] );
 		}
-		
+
 	}
-	
+
 	public function testIcalSubscribe()
 	{
 		$actual   = do_shortcode( '[eo_subscribe type="ical"]Subscribe[/eo_subscribe]' );
 		$expected = '<a href="http://example.org/?feed=eo-events" target="_blank"  title="Subscribe to calendar"  >Subscribe</a>';
 		$this->assertEquals( $expected, $actual );
 	}
-	
+
 	public function testGoogleSubscribe()
 	{
 		$actual   = do_shortcode( '[eo_subscribe type="google"]Subscribe[/eo_subscribe]' );
-		$expected = '<a href="httphttps://www.google.com/calendar/render?cid=http%3A%2F%2Fexample.org%2F%3Ffeed%3Deo-events" target="_blank"  title="Subscribe to calendar"  >Subscribe</a>';
+		$expected = '<a href="https://www.google.com/calendar/render?cid=http%3A%2F%2Fexample.org%2F%3Ffeed%3Deo-events" target="_blank"  title="Subscribe to calendar"  >Subscribe</a>';
 		$this->assertEquals( $expected, $actual );
 	}
 
@@ -54,7 +54,7 @@ class subscribeLinkTest extends EO_UnitTestCase
 		$_SERVER['HTTPS'] = 'on';
 		$actual   = do_shortcode( '[eo_subscribe type="google"]Subscribe[/eo_subscribe]' );
 
-		$expected = '<a href="httphttps://www.google.com/calendar/render?cid=http%3A%2F%2Fexample.org%2F%3Ffeed%3Deo-events" target="_blank"  title="Subscribe to calendar"  >Subscribe</a>';
+		$expected = '<a href="https://www.google.com/calendar/render?cid=http%3A%2F%2Fexample.org%2F%3Ffeed%3Deo-events" target="_blank"  title="Subscribe to calendar"  >Subscribe</a>';
 
 		//Disable SSL again
 		$_SERVER['HTTPS'] = 'off';
@@ -63,12 +63,12 @@ class subscribeLinkTest extends EO_UnitTestCase
 	}
 
 	public function testAttributes()
-	{	
+	{
 		$actual   = do_shortcode( '[eo_subscribe type="ical" id="subscribe" class="btn btn-primary" style="float:right" title="Subscribe" ]Subscribe to my calendar[/eo_subscribe]' );
 		$expected = '<a href="http://example.org/?feed=eo-events" target="_blank" class="btn btn-primary" title="Subscribe" id="subscribe" style="float:right">Subscribe to my calendar</a>';
 		$this->assertEquals( $expected, $actual );
 	}
-	
+
 	public function testCategory()
 	{
 		$cat_id = $this->factory->event_category->create( array(
@@ -92,4 +92,3 @@ class subscribeLinkTest extends EO_UnitTestCase
 		$this->assertEquals( $expected, $actual );
 	}
 }
-
