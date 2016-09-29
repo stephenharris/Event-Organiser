7dc6a644c12233f2151a30294b1c3d5406838f39
Stephen Harris <contact@stephenharris.info>
2016-09-29 20:39:47 +0100

    Automated tests: Symlink the build into the temporary WordPress install

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -122,7 +122,7 @@ install_test_suite
 install_db
 
 rm -rf ${WP_CORE_DIR}wp-content/plugins/*
-rsync -av --exclude=".*" $WORKSPACE/dist/event-organiser ${WP_CORE_DIR}wp-content/plugins/
+ln -s $WORKSPACE/dist/event-organiser ${WP_CORE_DIR}/wp-content/plugins/event-organiser
 
 # Used when waiting for stuff
 NAP_LENGTH=1
