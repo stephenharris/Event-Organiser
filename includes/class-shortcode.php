9262e0df947096e4251256ba43224df59eaeeb0c
Stephen Harris <contact@stephenharris.info>
2017-11-14 21:12:09 +0000

    Remove unused files.

--- includes/class-shortcode.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface EO_Shortcode {
-
-	public function set_attributes( $attributes );
-	public function set_content( $content = null );
-	public function render( $assets_lazy_loader );
-
-}
