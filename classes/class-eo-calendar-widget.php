6a3b84a9a257565201683f1b76ab325ee3da7501
ash-f <fujita@handyla.com>
2021-09-22 18:16:54 -0700

    Update class-eo-calendar-widget.php
    Small change

--- classes/class-eo-calendar-widget.php
+++ classes/class-eo-calendar-widget.php
@@ -376,9 +376,9 @@ class EO_Calendar_Widget extends WP_Widget {
 							$link,
 							$cell - $offset
 						);
-						if( in_array( 'eo-multi-day', $class ) ){ $mdevent = $calendar_events[$formated_date]; $mdclass = $class; $mdtitle = $titles; }
+						if( in_array( 'eo-multi-day', $class ) ){ $mdevent = $calendar_events[$formated_date]; $mdclass = $class; $mdtitles = $titles; }
 					} elseif ( isset($mdevent) ) {
-						$classes = implode( ' ', $mdclass ); $classes = str_replace( ' eo-multi-day', '', $classes);
+						$classes = implode( ' ', $mdclass );
 						$body .= sprintf( "<td $data class='%s'> <a title='%s' href='%s'> %s </a></td>", esc_attr( $classes ), esc_attr( $mdtitles ), $cell - $offset );
 						if( $mdevent[0]->EndDate==$formated_date ){ unset($mdevent); }
 					} else {
